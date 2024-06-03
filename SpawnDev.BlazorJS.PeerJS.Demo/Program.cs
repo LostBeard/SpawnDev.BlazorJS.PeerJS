0724925f8b4d19dd942259614282379b73db98d6
Todd Tanner <todd@spawndev.com>
2024-06-02 21:48:19 -0400

    Demo update

--- SpawnDev.BlazorJS.PeerJS.Demo/Program.cs
+++ SpawnDev.BlazorJS.PeerJS.Demo/Program.cs
@@ -1,8 +1,6 @@
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpawnDev.BlazorJS;
-using SpawnDev.BlazorJS.PeerJS;
 using SpawnDev.BlazorJS.PeerJS.Demo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -12,9 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 // Add SpawnDev.BlazorJS interop
 builder.Services.AddBlazorJSRuntime();
 
-// Load the PeerJS Javascript library. 
-// Can be called in a component instead if desired, or loaded using a <script> tag in the index.html
-await Peer.Init();
-
-// Run app using BlazorJSRunAsync extension method
+// Run app using BlazorJSRunAsync
 await builder.Build().BlazorJSRunAsync();
