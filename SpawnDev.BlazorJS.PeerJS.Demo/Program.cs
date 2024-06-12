ff8c88d9232655622c9259fe472be6ac658b284d
Todd Tanner <todd@spawndev.com>
2024-06-11 21:59:00 -0400

    Version 1.1.0. Updated SpawnDev.BlazorJS.

--- SpawnDev.BlazorJS.PeerJS.Demo/Program.cs
+++ SpawnDev.BlazorJS.PeerJS.Demo/Program.cs
@@ -2,13 +2,32 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpawnDev.BlazorJS;
 using SpawnDev.BlazorJS.PeerJS.Demo;
+using SpawnDev.BlazorJS.Reflection;
+using System.Security.Claims;
+using System.Text.Json.Serialization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
 // Add SpawnDev.BlazorJS interop
 builder.Services.AddBlazorJSRuntime();
 
+//builder.Services.AddJSRuntimeJsonOptions(o => o.ReferenceHandler  = ReferenceHandler.Preserve);
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+var host = builder.Build();
+await host.StartBackgroundServices();
+
+var JS = BlazorJSRuntime.JS;
+var claimsIdentity = new ClaimsIdentity("Custom", ClaimTypes.Name, ClaimTypes.Role);
+claimsIdentity.AddClaim(new Claim(ClaimTypes.Name, "Sterling"));
+claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "Administrator"));
+claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "User"));
+claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "Moderator"));
+
+var token = claimsIdentity.ToBase64();
+var claimsIdentityRB = token.Base64ToClaimsIdentity();
+
 // Run app using BlazorJSRunAsync
-await builder.Build().BlazorJSRunAsync();
+await host.BlazorJSRunAsync();
